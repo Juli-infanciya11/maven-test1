ddb5bbbce0d9f8ef02cf498b1935ed06fb5d5d4e
Juliyana11 <juliyanainfanciya@gmail.com>
2026-09-22 11:36:06 +0530

    JULIYANAI0001 - Added utility function to add two numbers

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -0,0 +1,12 @@
+package com.example.javamavenjunithelloworld;
+
+public class Utility {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void juliyana() {
+        System.out.println("I am Juliyana");
+    }
+}
